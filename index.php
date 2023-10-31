5f46c067444ef8b93587e4507cd14c7e86aa7eab
vigneshmrx <vigneshsundrani55@gmail.com>
2023-10-31 15:36:19 +0530

    some-small-changes

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <!-- <link rel="stylesheet" href="style.css"> -->
 
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap');
+
         * {
             margin: 0;
             box-sizing: border-box;
@@ -14,6 +17,7 @@
 
         body {
             padding: 5px;
+            font-family: "Roboto Mono";
         }
 
         #main-content-area {
@@ -25,10 +29,25 @@
         }
 
         #left-area {
-            width: 80vw;
+            width: 75vw;
             border: 1px solid;
             margin-right: 10px;
             margin-bottom: 10px;
+            padding: 25px 5px;
+
+            display: flex;
+        }
+
+        #column-one,
+        #column-two,
+        #column-three {
+            flex: 1;
+            padding: 10px 5px;
+        }
+
+        #column-two,
+        #column-one {
+            border-right: 5px solid;
         }
 
         /* #left-area {
@@ -37,7 +56,7 @@
         } */
 
         #right-area {
-            width: 20vw;
+            width: 25vw;
             border: 1px solid;
             margin-bottom: 10px;
         }
@@ -71,6 +90,35 @@
             width: 100%;
             display: grid;
             place-items: center;
+            padding: 15px;
+        }
+
+        #right-side-form {
+            width: 100%;
+            line-height: 30px;
+            position: relative;
+        }
+
+        #right-side-form input[type=text] {
+            width: 100%;
+            height: 30px;
+            padding: 3px 5px;
+            border-radius: 5px;
+            border-bottom: 1px solid;
+            border-right: none;
+            border-left: 1px solid;
+            border-top: none;
+            font-family: "Roboto Mono";
+        }
+
+        #right-side-form input[type=button],
+        input[type=submit] {
+            font-family: "Roboto Mono";
+        }
+
+        #add-task-submit-btn {
+            width: 100%;
+            height: 30px;  
         }
 
         #nav-section {
@@ -95,14 +143,42 @@
 <body>
     <section id="main-content-area">
         <div id="left-area">
-            dfdfd
+            <div id="column-one">
+                Blah
+            </div>
+            <div id="column-two">
+                Blah
+            </div>
+            <div id="column-three">
+                Blah
+            </div>
         </div>
         <div id="right-area">
             <div id="adding-list">
-                <form action="" method="post">
-                    Item: <br>
+                <form action="" method="post" id="right-side-form">
+                    Task: <br>
                     <input type="text" name="" id="" placeholder="Ex: Team meeting at 20:00"> <br>
-                    <input type="button" value="Add Item" onclick="addItem();">
+                    <input type="button" value="Add Task" onclick="addItem();" id="task1" style="margin-top: 10px;">
+                    <br><br>
+                    Heading (optional): <br>
+                    <input type="text" name="heading" placeholder="Ex: Business"> <br><br>
+                    Add to column: <br>
+
+                    <label for="col1">
+                        <input type="radio" name="cols" id="col1"> Column 1
+                    </label>
+
+                    <label for="col2">
+                        <input type="radio" name="cols" id="col2"> Column 2
+                    </label>
+
+                    <label for="col3">
+                        <input type="radio" name="cols" id="col3"> Column 3
+                    </label> 
+
+                    <br><br>
+
+                    <input type="submit" value="Add" name="add_tasks" id="add-task-submit-btn">
                 </form>
             </div>
         </div>
